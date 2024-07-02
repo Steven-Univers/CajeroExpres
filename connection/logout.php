c8bd66b03aee2b785c69d9c6b77caa5594693135
Steven <22393225@utcancun.edu.mx>
2024-07-02 11:36:33 -0500

    UPDATE/FINAL

--- connection/logout.php
+++ connection/logout.php
@@ -4,12 +4,14 @@ session_start();
 require_once './conn.php';
 
 if(isset($_SESSION['id'])){
-    $sql = "UPDATE tb_clientes SET estado = 'Inactivo' WHERE id_cliente = " .$_SESSION['id_cliente'];
-    //$id_cliente = $_SESSION['id_cliente'];
-    //$sql = "call cajero.sp_actualizarEstadoCliente_inactivo('$id_cliente')";
+    //$sql = "UPDATE tb_clientes SET estado = 'Inactivo' WHERE id_cliente = " .$_SESSION['id_cliente'];
+    $id_cliente = $_SESSION['id_cliente'];
+    $sql = "call sp_estado_inactivo('$id_cliente')";
     $conn->query($sql);
     session_destroy();
 }
 
 header("Location: ../index.html");
 ?>
+
+    $conexion->query($sql);
